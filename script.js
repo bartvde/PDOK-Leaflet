760bb76ca75070448320c2950aeaf2db0c0e114b
Bart van den Eijnden <bartvde@opengeo.org>
2012-12-15 13:51:10 +0100

    fix up L.Transformation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
-var RD = L.CRS.proj4js('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', new L.Transformation(1, 0, 1,0));
+var RD = L.CRS.proj4js('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', new L.Transformation(1, 285401.920, -1, 903401.920));
 var map = new L.Map('map', {
   continuousWorld: true,
   crs: RD,
@@ -15,6 +15,6 @@ var map = new L.Map('map', {
         continuousWorld: true
     })
   ],
-  center: new L.LatLng(51.9, 10),
+  center: new L.LatLng(52, 5.3),
   zoom: 3
 });
